60b473c256737103ab924bde37008bdf3517be80
zinia <zinia@mpower-social.com>
2019-10-28 11:26:12 +0600

    Generated table from json data after fetching from API

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,41 @@
 import * as React from 'react';
 import PokemonSearch from './components/PokemonSearch';
+import './components/pokemon.css';
 
 
 
 class App extends React.Component {
+
+  state = {
+    //datarecorrd
+    contacts: [] ,
+    //loading flag
+    loading: true,
+    error: false
+  }
+
+  componentDidMount = () => {
+    fetch('http://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then((data) => {
+      this.setState({ contacts: data,
+        loading: false})
+    }).catch(
+      error => this.setState({ 
+      loading: false, 
+      error: true 
+    }));
+  }
+
   public render() {
+    //loading data from state into local const variable
+    const { contacts, loading, error } = this.state;
     return (
       <div className="App">
-        <PokemonSearch name="Annu"/>
-
+        //if api data already loaded and no error then render table
+        {!loading && !error && <PokemonSearch contacts={contacts}/>
+        }
+        
       </div>
     );
   }
--- src/components/PokemonSearch.tsx
+++ src/components/PokemonSearch.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
-import Contacts from './Contacts';
+import './pokemon.css';
+// import Contacts from './Contacts';
 // import { Button } from 'reactstrap';
 
 
 export interface PokemonSearchProps {
-   name: string;
+    contacts: Object,
 }
  
 export interface PokemonSearchState {
@@ -15,7 +16,13 @@ class PokemonSearch extends React.Component<PokemonSearchProps, PokemonSearchSta
 
     
     state = { count : 0,
-        contacts: []}
+        students: [
+            { id: 1, name: 'Wasif', age: 21, email: 'wasif@email.com' },
+            { id: 2, name: 'Ali', age: 19, email: 'ali@email.com' },
+            { id: 3, name: 'Saad', age: 16, email: 'saad@email.com' },
+            { id: 4, name: 'Asad', age: 25, email: 'asad@email.com' }
+         ]
+    }
     
     multiply = () => {
         this.setState({count:(this.state.count*this.state.count)})
@@ -33,23 +40,77 @@ class PokemonSearch extends React.Component<PokemonSearchProps, PokemonSearchSta
         });
       };
 
-      componentDidMount = () => {
-        fetch('http://jsonplaceholder.typicode.com/users')
-        .then(res => res.json())
-        .then((data) => {
-          this.setState({ contacts: data })
-        }).catch(console.log)
+      
+
+
+      renderTableHeader =() =>{
+        // this.componentDidMount();
+          console.log(this.props.contacts);
+        let header = Object.keys(this.state.students[0])
+        return header.map((key, index) => {
+           return <th key={index}>{key.toUpperCase()}</th>
+        })
+     }
+     
+     renderTableData = () =>{
+        return this.state.students.map((student, index) => {
+           const { id, name, age, email } = student //destructuring
+           return (
+              <tr key={id}>
+                 <td>{id}</td>
+                 <td>{name}</td>
+                 <td>{age}</td>
+                 <td>{email}</td>
+              </tr>
+           )
+        })
+     }
+
+     renderContactTableHeader = ()=>{
+        let header = Object.keys(this.props.contacts[0])
+        return header.map((key, index) => {
+           return <th key={index}>{key.toUpperCase()}</th>
+        })
+     }
+
+     createTable = () => {
+        let contacts = this.props.contacts;
+        let table = [];
+        let len  = Object(contacts).length;
+        // Outer loop to create parent
+        for (let i = 0; i < len; i++) {
+          let children = []
+          let child = contacts[i];
+          //Inner loop to create children
+          let keys = Object.keys(child);
+          for (let key of keys) {
+              let childData = child[key];
+            if (typeof (childData) == 'object'){
+                childData = '';
+            }
+            children.push(<td key={key}>{childData}</td>)
+          }
+          //Create the parent and add the children
+          table.push(<tr key={i}>{children}</tr>)
+        }
+        return table
       }
 
     render() { 
         return ( <div>
             <h1>{this.state.count}</h1>
+            <div><select name="weight"><option value="1">Increased</option><option value="2">Decreased</option></select></div>
          
         <button name="1" onClick={this.componentDidMount}>Increment</button>
         <button name="2" onClick={this.decrement}>Decrement</button>
         <button name="3" onClick={this.multiply}>multiply</button>
-        <h2>{this.props.name}</h2>
-        <Contacts contacts={this.state.contacts} />
+        <h2>Table part</h2>
+        <table id = "contacts">
+        <tbody>
+                  <tr>{this.renderContactTableHeader()}</tr>
+                  {this.createTable()}
+               </tbody>
+        </table>
         
         </div>  );
     }
